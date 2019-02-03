0b6ca10ee7a599df2fcfd21c211c54ea56ee3780
Filip Stevanovic <filipus93@live.com>
2019-02-03 21:40:55 +0100

    dodati line breakovi za mail

--- resources/views/email/bill.blade.php
+++ resources/views/email/bill.blade.php
@@ -1,8 +1,10 @@
 <div>
-	Dear {{ $bill->stay->guest->first_name }} {{ $bill->stay->guest->last_name }},
-
+	<p>
+	Dear {{ $bill->stay->guest->first_name }} {{ $bill->stay->guest->last_name }},<br>
+	<br>
 	For your stay in our hotel from {{ $bill->stay->check_in_time }}
-	your bill is {{ $bill->amount }}
-
+	your bill is {{ $bill->amount }}<br>
+	<br>
 	Thank you for using our services
+	</p>
 </div>
